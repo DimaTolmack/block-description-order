6aeeb4f8cb26273ea5f9818f14865a391d7a1e3f
dtolm <dtolmachev998@gmail.com>
2024-01-18 16:01:16 +0300

    finish

---
+++ modal.js
@@ -0,0 +1,9 @@
+modal({
+    modalSelector: '.modal',
+    triggerSelector: '.column__link',
+    closeSelector: '.modal__button-closed',
+    modalSelectorDisplay: '(default: block)',
+    timeSelectorDisplay: '(default: block)',
+    timeShowModal: '(default: 60000)',
+    scrollSelectorDisplay: '(default: block)',
+});
